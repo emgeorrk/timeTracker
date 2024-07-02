c7b23e60dfb4f46937bdbda8ddeaa7dcc7fb35a3
Egor <nounstep@gmail.com>
2024-07-02 14:12:31 +0300

    Feature: models changed

--- models/models.go
+++ models/models.go
@@ -7,7 +7,8 @@ import (
 
 type User struct {
 	gorm.Model
-	PassportNumber string `json:"passportNumber"`
+	PassportSeries string `json:"passport_series"`
+	PassportNumber string `json:"passport_number"`
 	Surname        string `json:"surname"`
 	Name           string `json:"name"`
 	Patronymic     string `json:"patronymic"`
@@ -17,8 +18,14 @@ type User struct {
 
 type Task struct {
 	gorm.Model
-	UserID    uint      `json:"userId"`
-	Name      string    `json:"name"`
-	StartTime time.Time `json:"startTime"`
-	EndTime   time.Time `json:"endTime"`
+	UserID           uint          `json:"user_id"`
+	Name             string        `json:"name"`
+	IsActive         bool          `json:"is_active"`
+	Periods          []Period      `json:"periods"`
+	OverallTimeSpent time.Duration `json:"overall_time"`
+}
+
+type Period struct {
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
 }
