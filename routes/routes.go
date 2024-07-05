958e0fa4eae5e70d60f3457762e511dcb13d41bd
Egor <nounstep@gmail.com>
2024-07-05 19:29:43 +0300

    Refactor: routes format

--- routes/routes.go
+++ routes/routes.go
@@ -2,19 +2,30 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"timeTracker/app"
 	"timeTracker/handlers"
 )
 
-func InitRouter() *gin.Engine {
+func InitRouter(myApp *app.App) *gin.Engine {
 	r := gin.Default()
-	
-	v1 := r.Group("/")
-	
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", myApp.DB)
+	})
+
+	v1 := r.Group("/api/v1")
+
 	v1.GET("/users", handlers.GetUsers)
-	v1.GET("/users/:id", handlers.GetUserByID)
+	v1.GET("/users/:id/tasks_overview", handlers.GetTasksOverview)
+
 	v1.POST("/users", handlers.CreateUser)
+	v1.POST("/users/:id/tasks", handlers.CreateTask)
+	v1.POST("/users/:id/tasks/:task_id/start", handlers.StartTaskTimer)
+	v1.POST("/users/:id/tasks/:task_id/stop", handlers.StopTaskTimer)
+
 	v1.PUT("/users/:id", handlers.UpdateUser)
+
 	v1.DELETE("/users/:id", handlers.DeleteUser)
-	
+
 	return r
 }
