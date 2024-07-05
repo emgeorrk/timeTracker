0a733672396811e762df4feb81292353dff02d71
Egor <nounstep@gmail.com>
2024-07-05 16:02:51 +0300

    Fix: TaskID field in period

--- models/models.go
+++ models/models.go
@@ -1,32 +1,32 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
 	"time"
 )
 
 type User struct {
-	gorm.Model
+	ID             uint   `gorm:"primary_key" json:"id"`
 	PassportSeries string `json:"passport_series"`
 	PassportNumber string `json:"passport_number"`
 	Surname        string `json:"surname"`
 	Name           string `json:"name"`
 	Patronymic     string `json:"patronymic"`
 	Address        string `json:"address"`
-	Tasks          []Task `json:"tasks"`
+	Tasks          []Task `gorm:"foreignKey:UserID" json:"tasks"`
 }
 
 type Task struct {
-	gorm.Model
-	UserID           uint          `json:"user_id"`
+	ID               uint          `gorm:"primary_key;autoIncrement:false" json:"id"`
+	UserID           uint          `gorm:"primary_key;autoIncrement:false" json:"user_id"`
 	Name             string        `json:"name"`
 	IsActive         bool          `json:"is_active"`
-	Periods          []Period      `json:"periods"`
+	Periods          []Period      `gorm:"foreignKey:ID;References:ID,TaskID" json:"periods"`
 	OverallTimeSpent time.Duration `json:"overall_time"`
 }
 
 type Period struct {
-	gorm.Model
+	TaskID    uint      `gorm:"primary_key;autoIncrement:false" json:"task_id"`
+	UserID    uint      `gorm:"primary_key;autoIncrement:false" json:"user_id"`
 	StartTime time.Time `json:"start_time"`
 	EndTime   time.Time `json:"end_time"`
 }
